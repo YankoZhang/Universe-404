837b7ede26176636ac4c5b99b30e0933db21a3c9
shenyao3 <68770053+shenyao3@users.noreply.github.com>
2022-05-02 17:21:14 +0800

    Add files via upload
    先创建一个空project，拖这个代码，两个icon都选耳机

---
+++ universe 404/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public GameObject soundOnIcon;
+    public GameObject soundOffIcon;
+
+    private bool muted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
+    }
+
+    public void OnButtonPress()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (muted == false)
+            {
+                muted = true;
+                AudioListener.pause = true;
+            }
+
+            else
+            {
+                muted = false;
+                AudioListener.pause = false;
+            }
+        }
+            
+        Save();
+        UpdateButtonIcon();
+    }
+
+
+    private void UpdateButtonIcon()
+    {
+        if(muted == false)
+        {
+            soundOnIcon.SetActive(true);
+            soundOffIcon.SetActive(false);
+        }
+        else
+        {
+            soundOnIcon.SetActive(false);
+            soundOffIcon.SetActive(true);
+        }
+    }
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 0;
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
